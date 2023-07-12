59a9d543aee59934f33dfafadd5e3086cab4bfea
hackchele <iancruzcolella@gmail.com>
2023-07-11 22:15:52 -0300

    pagina carrito y login sin terminar

--- app.js
+++ app.js
@@ -8,9 +8,19 @@ app.get("/", (req, res) => {
     res.sendFile(ruta);
 });
 
+app.get("/login", (req, res) => {
+    const ruta = path.join(__dirname, "./views/login.html");
+    res.sendFile(ruta);
+});
+
+app.get("/carrito", (req, res) => {
+    const ruta = path.join(__dirname, "./views/carrito.html");
+    res.sendFile(ruta);
+});
+
 const publicFolder = path.resolve(__dirname, "./public");
 app.use(express.static(publicFolder));
 
-app.listen(3030, () =>{
-    console.log("El servidor esta escuchando en el 3030");
+app.listen(3010, () =>{
+    console.log("El servidor esta escuchando en el 3010");
 });
