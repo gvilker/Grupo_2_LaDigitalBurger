3c1be4cf24db156789121fc7e1bada616784dd81
GabrielCano89 <canogabriel89@gmail.com>
2023-07-22 17:28:34 -0300

    app.js modificado, archivo.env, resolve

--- app.js
+++ app.js
@@ -1,31 +1,35 @@
-const { log } = require("console");
+
 const express = require("express");
-const path = require("path");
 const app = express();
 
+const path = require("path");
+const dotenv = require ("dotenv").config();
+const port = process.env.PORT ||3010;
+
+app.listen(port, () =>{
+    console.log("El servidor esta escuchando en el puerto" + port + "http://localhost:" + port);
+});
+
 app.get("/", (req, res) => {
-    const ruta = path.join(__dirname, "./views/home.html");
-    res.sendFile(ruta);
+    const ruta = "./views/home.html";
+    res.sendFile(path.resolve(__dirname, ruta))
 });
 
 app.get("/login", (req, res) => {
-    const ruta = path.join(__dirname, "./views/login.html");
-    res.sendFile(ruta);
+    const ruta = "./views/login.html";
+    res.sendFile(path.resolve(__dirname, ruta))
 });
 
 app.get("/Register", (req, res) => {
-    const ruta = path.join(__dirname, "./views/register.html");
-    res.sendFile(ruta);
+    const ruta = "./views/register.html";
+    res.sendFile(path.resolve(__dirname, ruta))
 });
 
 app.get("/carrito", (req, res) => {
-    const ruta = path.join(__dirname, "./views/carrito.html");
-    res.sendFile(ruta);
+    const ruta = "./views/carrito.html";
+    res.sendFile(path.resolve(__dirname, ruta))
 });
 
 const publicFolder = path.resolve(__dirname, "./public");
 app.use(express.static(publicFolder));
 
-app.listen(3010, () =>{
-    console.log("El servidor esta escuchando en el 3010");
-});
