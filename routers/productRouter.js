d73086a5cfc0f3b4f36176274c0f31a4ef3886cc
GabrielCano89 <canogabriel89@gmail.com>
2023-08-01 02:47:16 -0300

    productoDetail.ejs agregado y sus rutas, produto.ejs modificado

--- routers/productRouter.js
+++ routers/productRouter.js
@@ -2,6 +2,8 @@ const express = require ("express");
 const router = express.Router();
 const productController = require ("../controllers/productControllers");
 
+
 router.get("/producto", productController.producto);
+router.get("/productoDetail/:id", productController.productoDetail);
 
 module.exports = router;
