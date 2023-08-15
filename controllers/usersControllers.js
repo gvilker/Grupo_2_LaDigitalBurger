6024e01b1cbdc56b359035d892263140d144fd8b
GabrielCano89 <canogabriel89@gmail.com>
2023-08-15 00:26:34 -0300

    validacion completa register

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,4 +1,5 @@
 const path = require ("path");
+const { validationResult } = require('express-validator')
 
 const controller = {
     login: (req, res) => {
@@ -6,6 +7,21 @@ const controller = {
     },
     register: (req, res) => {
         res.render("register");
+    },
+    processRegister: (req,res) => {
+        let resultValidation = validationResult(req);   
+
+        if (resultValidation.errors.length > 0) {
+            return res.render('register', {
+                errors: resultValidation.mapped(),
+                oldData: req.body
+            });
+        }
+
+        return res.send('las validaciones se pasaron correctamente')
+    },
+    profile: (req, res) => {
+        res.render("userProfile")
     }
 }
 
--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -1,8 +1,61 @@
 const express = require ("express");
 const router = express.Router();
+
+
+const path = require('path');
+const multer = require('multer');
+
+const { body } = require('express-validator');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './public/images/avatars');
+    },
+    filename: (req, file, cb) => {
+        const fileName = `${Date.now()}_img${path.extname(file.originalname)}`;
+        cb(null, fileName);
+    }    
+})
+
+const uploadFile = multer({ storage });
+
+
 const usersController = require ("../controllers/usersControllers");
 
-router.get("/login", usersController.login);
+const validations = [
+    body('nombre_completo').notEmpty().withMessage('Escribe un nombre'),
+    body('nombre_usuario').notEmpty().withMessage('Ingrese un alias'),
+    body('correo_electronico')
+        .notEmpty().withMessage('Escribe tu correo electrónico').bail()
+        .isEmail().withMessage('Debes escribir un formato correro válido'),
+    body('contrasena').notEmpty().withMessage('Debes proporcionar una contraseña'),
+    body('confirmar_contrasena').notEmpty().withMessage('Repita su contraseña'),
+    body('avatar').custom((value, {req}) => {
+        let file = req.file;
+        let acceptedExtensions = ['.jpg', '.png', '.jpeg', '.gif']
+
+        if (!file){
+            throw new Error('Tienes que subir una imagen')
+        } else{
+            let fileExtension = path.extname(file.originalname);
+            if (!acceptedExtensions.includes(fileExtension)){
+                throw new Error(`Las extensiones de archivo permitidas son ${acceptedExtensions.join(', ')}`);
+        }       
+    }
+        return true;
+    })
+]
+
+// Formulario de registro
 router.get("/register", usersController.register);
+
+// Procesar el registro
+router.post("/register", uploadFile.single('avatar'), validations,  usersController.processRegister);
+
+// Formulario de login
+router.get("/login", usersController.login);
+
+// Perfil de usuario
+router.get('/profile/:userId', usersController.profile)
 
 module.exports = router;
