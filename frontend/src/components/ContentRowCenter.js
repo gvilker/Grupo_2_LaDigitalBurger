ef49ed4acbd0b54b79a4e8f85f7d4d9dbd7bbe58
gvilker <gvilker@yahoo.com>
2023-11-11 16:47:04 -0300

    otros

--- frontend/src/components/ContentRowCenter.js
+++ frontend/src/components/ContentRowCenter.js
@@ -6,11 +6,9 @@ function ContentRowCenter(){
     return (
         <div className="row">
             
-            {/*<!-- Last Movie in DB -->*/}
+           
             <LastProductInDb />
-            {/*<!-- End content row last movie in Data Base -->*/}
-
-            {/*<!-- Genres in DB -->*/}
+           
             <GenresInDb />
 
         </div>
